0bbc2d6ec77d39ed880df0858a499dd384f5d77f
HemantPaul <hemantpaul3011@gmail.com>
2026-06-18 23:19:07 +0530

    1431

---
+++ 1431_kidcandy.c
@@ -0,0 +1,24 @@
+bool* kidsWithCandies(int* candies,
+                      int candiesSize,
+                      int extraCandies,
+                      int* returnSize)
+{
+    *returnSize = candiesSize;
+
+    bool* result = (bool*)malloc(candiesSize * sizeof(bool));
+
+    int max = candies[0];
+
+    for(int i = 1; i < candiesSize; i++)
+    {
+        if(candies[i] > max)
+            max = candies[i];
+    }
+
+    for(int i = 0; i < candiesSize; i++)
+    {
+        result[i] = (candies[i] + extraCandies >= max);
+    }
+
+    return result;
+}
