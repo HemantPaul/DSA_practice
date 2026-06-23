c9600b8b90c0915a62bbf580e6d165bf4d43b9e7
HemantPaul <hemantpaul3011@gmail.com>
2026-06-23 23:54:53 +0530

    392

---
+++ 392_issubsequence.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool isSubsequence(char* s, char* t) {
+    int i = 0; // pointer for s
+    int j = 0; // pointer for t
+
+    while (s[i] != '\0' && t[j] != '\0') {
+        if (s[i] == t[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return s[i] == '\0';
+}
